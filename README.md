3edd3188d970a274ff872e93c08c6365be36f6bb
Shahriar Kasem <shahriarkasem@gmail.com>
2022-04-05 02:27:00 +0600

    project done

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ Click here: https://m-55-product-analysis-website-shahriarkasem.netlify.app/
 Click here: https://github.com/programming-hero-web-course-4/product-analysis-website-shahriarkasem 
 
 ### Notes:
-`In this website you can see our business details chart in Dashboard. You can check our customer reviews. For getting more information about our products you can check out our Blog page. If you want our contact information check Contact page.`
+`In this website you can see our business details chart in Dashboard. You can check our customer reviews. For getting more information about our products you can check out our Blog page. If you want our contact information check About page.`
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>This is About.</h1>
+        <div className='mx-5'>
+            <h1 className='text-center text-red-500 font-bold text-2xl my-4'>Hey! We are here to help you with more information.</h1>
+            <h5 className='text-center font-semibold text-xl my-2'>Our website info:</h5>
+            <p>In this website you can see our business details chart in Dashboard. You can check our customer reviews. For getting more information about our products you can check out our Blog page. You can contact us on <span className='text-red-500 font-medium'>Twitter, Facebook</span> or you can mail us at <span className='text-red-500 font-medium'>'sportsworld@worldsport.com'</span>.</p>
         </div>
     );
 };
