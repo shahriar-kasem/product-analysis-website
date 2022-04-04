1368568fa82f64178b2ffe29a168fcc093bbd843
Shahriar Kasem <shahriarkasem@gmail.com>
2022-04-05 00:51:06 +0600

    working on rechart

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 import useBusinessData from '../../hooks/useBusinessData';
 
 const Dashboard = () => {
@@ -8,7 +9,15 @@ const Dashboard = () => {
         <div className='dashboard-container'>
             <h1>This is Dashboard. Total data: {data.length}</h1>
             <div className='rechart-container'>
-                <div></div>
+                <div>
+                    <h3>Month wise sell</h3>
+                    <LineChart width={500} height={300} data={data}>
+                        <XAxis dataKey='month'></XAxis>
+                        <YAxis></YAxis>
+                        <Tooltip></Tooltip>
+                        <Line type="monotone" dataKey='sell'></Line>
+                    </LineChart>
+                </div>
                 <div></div>
                 <div></div>
                 <div></div>
