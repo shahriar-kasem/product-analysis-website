9078091f2fea942c943da7da7a4634ca2318bbc0
Shahriar Kasem <shahriarkasem@gmail.com>
2022-04-04 07:39:16 +0600

    react router added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h1 className='text-5xl font-bold'>Welcome to our new World!!!</h1>
+        <div className='header-container pb-2'>
+            <div>
+                <h1 className='text-5xl font-bold p-1 mb-2'>Welcome to our new World!!!</h1>
+            </div>
+            <div className='link-container'>
+                <Link to='/'>Home</Link>
+                <Link to='/reviews'>Reviews</Link>
+                <Link to='/dashboard'>Dashboard</Link>
+                <Link to='/blogs'>Blogs</Link>
+                <Link to='/about'>About</Link>
+            </div>
         </div>
     );
 };
