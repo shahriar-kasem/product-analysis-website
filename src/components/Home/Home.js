f6d69d4b3fc0346048e25a639ea7a48af8d96116
Shahriar Kasem <shahriarkasem@gmail.com>
2022-04-04 11:19:10 +0600

    little responsive

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,8 +6,8 @@ const Home = () => {
         <div className='home-container md:p-2 p-5'>
             <section className='home-main grid grid-cols-1 md:grid-cols-2'>
                 <div className='home-main-details md:ml-10 ml-0'>
-                    <h2 className='text-center text-orange-500 text-4xl font-medium'>Our best is your best!</h2>
-                    <h1 className='text-center text-orange-300 text-2xl font-medium  m-3 '>Choose quality products</h1>
+                    <h2 className='text-center text-orange-500 xl:text-6xl lg:text-5xl text-3xl font-medium'>Our best is your best!</h2>
+                    <h1 className='text-center text-orange-300 xl:text-5xl lg:text-5xl text-2xl font-medium  m-5 '>Choose quality products</h1>
                     <p className=''>Shop a wide collection of sports clothes and equipments. We don't compromise with our product qualities! We are promised to give our customers best things. Their satisfaction is our goal.</p>
                     <br />
                     <div className='ml-2'>
