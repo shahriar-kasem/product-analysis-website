636b6a6eabca87d2c9601d8a57e9753b17a60bf2
Shahriar Kasem <shahriarkasem@gmail.com>
2022-04-05 01:48:12 +0600

    dashboard 75% done

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Area, AreaChart, Bar, BarChart, Line, LineChart, Pie, PieChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import useBusinessData from '../../hooks/useBusinessData';
 
 const Dashboard = () => {
@@ -8,19 +8,68 @@ const Dashboard = () => {
     return (
         <div className='dashboard-container'>
             <h1>This is Dashboard. Total data: {data.length}</h1>
-            <div className='rechart-container'>
-                <div>
-                    <h3>Month wise sell</h3>
-                    <LineChart width={500} height={300} data={data}>
-                        <XAxis dataKey='month'></XAxis>
-                        <YAxis></YAxis>
-                        <Tooltip></Tooltip>
-                        <Line type="monotone" dataKey='sell'></Line>
-                    </LineChart>
+            <div className='rechart-container grid grid-cols-2'>
+                <div className='line-chart'>
+                    <h3 className='text-center text-2xl'>Month wise sell</h3>
+                    <ResponsiveContainer width="80%" height={400}>
+                        <LineChart data={data} margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}>
+                            <XAxis dataKey='month'></XAxis>
+                            <YAxis></YAxis>
+                            <Tooltip></Tooltip>
+                            <Line type="monotone" dataKey='sell'></Line>
+                        </LineChart>
+                    </ResponsiveContainer>
+
                 </div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div className=''>
+                    <h3>Investment VS Revenue</h3>
+                    <ResponsiveContainer width="80%" height={400}>
+                        <AreaChart data={data} margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}>
+                            <XAxis dataKey='month'></XAxis>
+                            <YAxis></YAxis>
+                            <Tooltip></Tooltip>
+                            <Area dataKey='investment' fill="#8884d8"></Area>
+                            <Area dataKey='revenue' fill="#82ca9d"></Area>
+                        </AreaChart>
+                    </ResponsiveContainer>
+                </div>
+                <div>
+                    <ResponsiveContainer width="80%" height={400}>
+                        <BarChart data={data} margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}>
+                            <XAxis dataKey='month'></XAxis>
+                            <YAxis></YAxis>
+                            <Tooltip></Tooltip>
+                            <Bar dataKey='investment' fill="#8884d8" stackId="a"></Bar>
+                            <Bar dataKey='revenue' fill="#82ca9d" stackId="a"></Bar>
+                        </BarChart>
+                    </ResponsiveContainer>
+                </div>
+                <div>
+                    {/* <ResponsiveContainer width="100%" height="100%">
+                            <PieChart width={400} height={400}>
+                            <XAxis dataKey='month'></XAxis>
+                            <YAxis></YAxis>
+                            <Tooltip></Tooltip>
+                            <Pie data={data} dataKey="investment"></Pie>
+                            <Pie data={data} dataKey="revenue"></Pie>
+                            </PieChart>
+                        </ResponsiveContainer> */}
+                </div>
             </div>
         </div>
     );
