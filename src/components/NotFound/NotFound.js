280679ffd14915a0f4ea31a91db28eeb7233af83
Shahriar Kasem <shahriarkasem@gmail.com>
2022-04-04 13:49:11 +0600

    NotFound component added

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='text-center mt-10'>
+            <h1 className='text-red-500 text-5xl font-bold'>Not Found! 404</h1>
+            <p className='text-red-400 text-3xl font-semibold mt-2'>The page you are looking for is not available.</p>
+        </div>
+    );
+};
+
+export default NotFound;
