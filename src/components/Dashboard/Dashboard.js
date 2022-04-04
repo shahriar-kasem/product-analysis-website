9751b0a746c599146dcc1935ee59f4427285b5d9
Shahriar Kasem <shahriarkasem@gmail.com>
2022-04-04 23:21:43 +0600

    data loaded for chart

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import useBusinessData from '../../hooks/useBusinessData';
 
 const Dashboard = () => {
+    const [data] = useBusinessData([]);
+
     return (
-        <div>
-            <h1>This is Dashboard.</h1>
+        <div className='dashboard-container'>
+            <h1>This is Dashboard. Total data: {data.length}</h1>
+            <div className='rechart-container'>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
         </div>
     );
 };
